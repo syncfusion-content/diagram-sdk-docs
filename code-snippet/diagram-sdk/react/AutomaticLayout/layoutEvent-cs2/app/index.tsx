fca79d5f219b098e1d9cd777c17709d966a563e4
ChellaDhuraiSSF4055 <124353845+ChellaDhuraiSSF4055@users.noreply.github.com>
2026-08-18 17:21:51 +0530

    1038268: moved changes from diagram repo

--- code-snippet/diagram-sdk/react/AutomaticLayout/layoutEvent-cs2/app/index.tsx
+++ code-snippet/diagram-sdk/react/AutomaticLayout/layoutEvent-cs2/app/index.tsx
@@ -7,75 +7,113 @@ import { DiagramComponent, Inject, HierarchicalTree, LayoutAnimation, ConnectorM
 
 //Initialize nodes 
 let nodes: NodeModel[] = [
-  {
-    id: 'Start',
-    width: 140,
-    height: 50,
-    offsetX: 300,
-    offsetY: 50,
-    annotations: [{ content: 'Node1' }],
-    style: { fill: '#6BA5D7', strokeColor: 'white' },
-    expandIcon: {
-      shape: 'ArrowDown',
-      width: 20,
-      height: 15,
+    {
+        id: 'Node1',
+        width: 140,
+        height: 50,
+        offsetX: 300,
+        offsetY: 50,
+        annotations: [{ content: 'Node1' }],
+        style: { fill: '#6BA5D7', strokeColor: 'white' },
     },
-    collapseIcon: {
-      shape: 'ArrowUp',
-      width: 20,
-      height: 15,
+    {
+        id: 'Node2',
+        width: 140,
+        height: 50,
+        offsetX: 200,
+        offsetY: 140,
+        style: { fill: '#6BA5D7', strokeColor: 'white' },
+        annotations: [{ content: 'Node2' }],
     },
-  },
-  {
-    id: 'Init',
-    width: 140,
-    height: 50,
-    offsetX: 300,
-    offsetY: 140,
-    style: { fill: '#6BA5D7', strokeColor: 'white' },
-    annotations: [{ content: 'Node2' }],
-  },
-  {
-    id: 'Init2',
-    width: 140,
-    height: 50,
-    offsetX: 100,
-    offsetY: 140,
-    style: { fill: '#6BA5D7', strokeColor: 'white' },
-    annotations: [{ content: 'Node3' }],
-  },
-  {
-    id: 'Init3',
-    width: 140,
-    height: 50,
-    offsetX: 150,
-    offsetY: 140,
-    style: { fill: '#6BA5D7', strokeColor: 'white' },
-    annotations: [{ content: 'Node4' }],
-  }
+    {
+        id: 'Node4',
+        width: 140,
+        height: 50,
+        offsetX: 520,
+        offsetY: 140,
+        style: { fill: '#6BA5D7', strokeColor: 'white' },
+        annotations: [{ content: 'Node4' }],
+    },
+    {
+        id: 'Child1',
+        width: 140,
+        height: 50,
+        offsetX: 150,
+        offsetY: 230,
+        style: { fill: '#6BA5D7', strokeColor: 'white' },
+        annotations: [{ content: 'Child1' }],
+    },
+    {
+        id: 'Child2',
+        width: 140,
+        height: 50,
+        offsetX: 300,
+        offsetY: 230,
+        style: { fill: '#6BA5D7', strokeColor: 'white' },
+        annotations: [{ content: 'Child2' }],
+    },
+    {
+        id: 'Child3',
+        width: 140,
+        height: 50,
+        offsetX: 450,
+        offsetY: 230,
+        style: { fill: '#6BA5D7', strokeColor: 'white' },
+        annotations: [{ content: 'Child3' }],
+    },
+    {
+        id: 'Child4',
+        width: 140,
+        height: 50,
+        offsetX: 600,
+        offsetY: 230,
+        style: { fill: '#6BA5D7', strokeColor: 'white' },
+        annotations: [{ content: 'Child4' }],
+    }
 ];
 
-//Initializes connectors
 let connectors: ConnectorModel[] = [
-  {
-    id: 'connector1',
-    sourceID: 'Start',
-    targetID: 'Init',
-    type: 'Orthogonal',
-  },
-  {
-    id: 'connector2',
-    sourceID: 'Start',
-    targetID: 'Init2',
-    type: 'Orthogonal',
-  },
-  {
-    id: 'connector3',
-    sourceID: 'Init2',
-    targetID: 'Init3',
-    type: 'Orthogonal',
-  }
+    {
+        id: 'connector1',
+        sourceID: 'Node1',
+        targetID: 'Node2',
+        type: 'Orthogonal',
+    },
+    {
+        id: 'connector2',
+        sourceID: 'Node1',
+        targetID: 'Node4',
+        type: 'Orthogonal',
+    },
+    {
+        id: 'connector3',
+        sourceID: 'Node2',
+        targetID: 'Child1',
+        type: 'Orthogonal',
+    },
+    {
+        id: 'connector5',
+        sourceID: 'Node2',
+        targetID: 'Child2',
+        type: 'Orthogonal',
+    },
+    {
+        id: 'connector6',
+        sourceID: 'Node4',
+        targetID: 'Child3',
+        type: 'Orthogonal',
+    },
+    {
+        id: 'connector7',
+        sourceID: 'Node4',
+        targetID: 'Child4',
+        type: 'Orthogonal',
+    }
 ];
+
+const layout = {
+    type: 'HierarchicalTree',
+};
 
 export default function App() {
   let diagramInstance: DiagramComponent;
@@ -101,9 +139,10 @@ export default function App() {
         // Event Triggers when the animation complete
         animationComplete={animationComplete}
         //Uses layout to auto-arrange nodes on the diagram page
-        layout={{
-          //Sets layout type
-          type: 'HierarchicalTree',
+        layout={layout}
+        getNodeDefaults={(node: NodeModel) => {
+          node.expandIcon = { shape: 'ArrowDown', width: 20, height: 15, };
+          node.collapseIcon = { shape: 'ArrowUp', width: 20, height: 15, };
         }}
       >
 
